00febfd36c1b773eddceb20bf0488cf7dfd58b93
Ayat Esaam <eayatessam1@gmail.com>
2026-03-18 23:38:55 +0200

    update

--- src/app/add-product-componet/add-product-componet.component.ts
+++ src/app/add-product-componet/add-product-componet.component.ts
@@ -1,34 +1,40 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product';
+import { IProduct } from '../models/iproduct';
 
 @Component({
   selector: 'app-add-product-componet',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-product-componet.component.html',
   styleUrl: './add-product-componet.component.css',
 })
 export class AddProductComponetComponent {
-   constructor(
+  
+  productForm: FormGroup;  
+
+  constructor(
     private fb: FormBuilder,
     private productService: ProductService,
     private router: Router
-  ) {}
-
-  productForm = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    price: [0, [Validators.required, Validators.min(0)]],
-    quantity: [1, [Validators.required, Validators.min(0)]],
-    categoryId: [null, Validators.required],
-    description: [''],
-    imageUrl: ['']
-  });
+  ) {
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: [0, [Validators.required, Validators.min(0)]],
+      quantity: [1, [Validators.required, Validators.min(0)]],
+      categoryId: [null, Validators.required],
+      description: [''],
+      imageUrl: [''],
+      date: [new Date().toISOString()]
+    });
+  }
 
   onSubmit() {
     if (this.productForm.valid) {
-      this.productService.add(this.productForm.value)
+      const newProduct = this.productForm.value as Omit<IProduct, 'id'>;
+      this.productService.add(newProduct)
         .subscribe(() => {
           this.router.navigate(['/products']);
         });
